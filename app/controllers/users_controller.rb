7fc8f5bf5c8f29b7cbfe5ce2a97b420752b8c2a6
Tokiopro <tokisaonana@yahoo.co.jp>
2022-04-25 08:44:57 +0000

    user一覧ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @users = User.all
+  end
   def show
     @user = User.find(params[:id])
     @books = @user.books
