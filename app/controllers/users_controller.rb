b8ee892f6a19fbc04877fb1161b673bc9df6c076
Tokiopro <tokisaonana@yahoo.co.jp>
2022-04-26 10:29:31 +0000

    edit,show controller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
+      if @user == current_user
+        render :edit
+      else
+        redirect_to user_path(current_user.id)
+      end
   end
   
   def update
